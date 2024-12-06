ccef9c7cf1823db2ee83fdf81d054b8f93a1131a
madnan-github <dononlineboss@gmail.com>
2024-12-07 00:49:16 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="text-blue-500 text-2xl text-center">Home</h1>
     </main>
   );
 }
